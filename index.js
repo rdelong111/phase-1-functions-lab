9ec53bbee526c77c00e51b9445026f54382bb6a5
RyanDeLong <rdelong111@gmail.com>
2021-12-17 19:41:37 -0600

    commit tree 7b6a9b08d34e08120b02286332d048bec8749533

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(x) {
+	return Math.abs(42 - x);
+}
+
+function distanceFromHqInFeet(x) {
+	return distanceFromHqInBlocks(x) * 264;
+}
+
+function distanceTravelledInFeet (x, y) {
+	return Math.abs(x - y) * 264;
+}
+
+function calculatesFarePrice(x, y) {
+	const disnft = distanceTravelledInFeet(x, y);
+
+	if (disnft <= 400) {
+		return 0;
+	}
+	else if (disnft > 400 && disnft <= 2000) {
+		return (disnft - 400) * 0.02;
+	}
+	else if (disnft > 2000 && disnft <= 2500) {
+		return 25;
+	}
+	else {
+		return 'cannot travel that far';
+	}
+}
